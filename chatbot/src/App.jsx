15bff4ab4feed494c0cac7823e8a63a6654d0b67
unknown <zelalemyohannes75@gmail.com>
2023-12-11 11:41:14 +0300

    add files

---
+++ chatbot/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import {  
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  TypingIndicator
+} from '@chatscope/chat-ui-kit-react'
+
+
+function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello I am ChatGPT!",
+      sender: "ChatGPT!"
+    }
+  ])
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message,
+      direction: 'outgoing',
+      sender: "user"
+  };
+
+  const newMessages = [...messages, newMessage];
+
+  setMessages(newMessages);
+
+}
+
+
+
+  return (
+    <div className='App'>
+      <div style={{ position:"relative", height: "800px", width: "700px"}}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList>
+              {
+                messages.map((message, i) => {
+                  return <Message key ={i} model={message} />
+                })
+              }
+            </MessageList>
+            <MessageInput placeholder='Type message here' onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+
+      </div>
+    </div>
+ 
+    )
+}
+
+export default App
